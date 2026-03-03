f5115c6c9d2202ffc1f76d1cd5d31344aa015dd7
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-03-03 20:52:34 +0700

    deklarasi global dan implementasi

---
+++ file1.cpp
@@ -0,0 +1,13 @@
+// library
+#include <iostream>
+using namespace std;
+
+// deklarasi global
+int p, l, luas;
+
+//implementasi prosedur dan fungsi
+void input() {
+    cout << "masukan panjang =";
+    cin >> p;
+    cout << "masukan lebar =";
+    cin >> l;
