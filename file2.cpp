f04498c0e1410d3070614979ce7bcc0aa38108b8
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-03-03 20:58:10 +0700

    deklarasi global file2

--- file2.cpp
+++ file2.cpp
@@ -4,3 +4,15 @@ using namespace std;
 
 //deklarasi global
 int p, l;
+
+//implementasi prosedur dan fungsi
+void input() {
+    cout << "masukkan panjang =";
+    cin >> p;
+    cout << "masukkan lebar =";
+    cin >> l;
+}
+
+float jumlah(float a, float b,float c) {
+    return a + b + c;
+}
